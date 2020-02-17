0ce9721b890455f487b02f6343517abd253ea4b1
Ashley McGlone <ashley@mcglone.com>
2020-02-17 10:30:58 -0500

    Do not import private directory until it is there

--- PSPolicy.psm1
+++ PSPolicy.psm1
@@ -1,6 +1,8 @@
 
-@("$PSScriptRoot\Private","$PSScriptRoot\Public") |
-    ForEach-Object {Get-ChildItem -Path "$_\*.ps1"} |
+#@("$PSScriptRoot\Private","$PSScriptRoot\Public") |
+
+@("$PSScriptRoot\Public") |
+ForEach-Object {Get-ChildItem -Path "$_\*.ps1"} |
     ForEach-Object {. $_.Fullname}
 
 Export-ModuleMember -Function *-*
